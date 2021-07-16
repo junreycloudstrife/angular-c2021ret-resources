ee04591f66ced6666b8d0e16fa0ecc45287e1018
junreycloudstrife <junrey.cloudstrife@gmail.com>
2021-07-16 23:30:32 +0800

    Activity 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-c2021ret-resources';
+  title = 'Jun Rey Ansing';
 }
